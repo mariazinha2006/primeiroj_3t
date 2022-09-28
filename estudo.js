82b7f8809312c2817b548da9ae4f431f5933db0a
mariazinha2006 <106184152+mariazinha2006@users.noreply.github.com>
2022-09-28 17:43:11 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,2 +1,17 @@
-console.log("Console: Alô Mundo...");
-alert("Alerta: Alô mundo...");
+//console.log("Console: Alô Mundo...");
+//alert ("alertaÇ: alo mundo...")
+let TABUADA = 5;
+
+function meChame(){
+  document.write("tabuada do" + TABUADA + "<br>")
+document.write( TABUADA+ "x 1 = " + (TABUADA*1) + "<br>"  );
+document.write( TABUADA+ "x 2 = " + (TABUADA*2) + "<br>"  );
+document.write( TABUADA+ "x 3 = " + (TABUADA*3) + "<br>"  );
+document.write( TABUADA+ "x 4 = " + (TABUADA*4) + "<br>"  );
+document.write( TABUADA+ "x 5 = " + (TABUADA*5) + "<br>"  );
+document.write( TABUADA+ "x 6 = " + (TABUADA*6) + "<br>"  );
+document.write( TABUADA+ "x 7 = " + (TABUADA*7) + "<br>"  );
+document.write( TABUADA+ "x 8 = " + (TABUADA*8) + "<br>"  );
+document.write( TABUADA+ "x 9 = " + (TABUADA*9) + "<br>"  );
+document.write( TABUADA+ "x 10 = " + (TABUADA*10) + "<br>"  );
+}
